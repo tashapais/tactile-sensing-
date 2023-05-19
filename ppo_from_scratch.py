69f418b6af92554c91c8d1e150364eba01f9a9b9
Nikhilesh Belulkar <nb2953@columbia.edu>
2023-05-19 19:47:15 -0400

    multiprocessing added

--- ppo_from_scratch.py
+++ ppo_from_scratch.py
@@ -3,12 +3,16 @@ import argparse
 import random
 import numpy as np
 import gym
-
+from grid_world_env import GridWorldEnv
+from ppo_discrete import VecPyTorch
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=1,
         help="seed of the experiment")
+    parser.add_argument('--multiprocess', type=lambda x: bool(strtobool(x)), 
+                        default=True, nargs='?', const=True)
     parser.add_argument("--num-envs", type=int, default=4,
         help="the number of parallel game environments")
     parser.add_argument("--num-steps", type=int, default=128,
@@ -39,19 +43,17 @@ def parse_args():
         help="the maximum norm for the gradient clipping")
     parser.add_argument("--target-kl", type=float, default=None,
         help="the target KL divergence threshold")
+
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
 
     return args
 
-def make_env(env_id, seed, idx, capture_video, run_name):
+def make_env(seed, idx):
     def thunk():
-        env = gym.make(env_id)
+        env = GridWorldEnv()
         env = gym.wrappers.RecordEpisodeStatistics(env)
-        if capture_video:
-            if idx == 0:
-                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
         env.seed(seed)
         env.action_space.seed(seed)
         env.observation_space.seed(seed)
@@ -66,4 +68,11 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
 
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    envs = gym.vector.SyncVectorEnv([make_env(args.env_id, args.seed + i, i, args.capture_video, run_name) for i in range(args.num_envs)])
+    if args.multiprocess:
+        envs = VecPyTorch(
+            SubprocVecEnv([make_env(args.seed + i, i)
+                           for i in range(args.num_envs)], "fork"), device)
+    else:
+        envs = VecPyTorch(
+            DummyVecEnv([make_env(args.seed + i, i)
+                         for i in range(args.num_envs)]), device)
